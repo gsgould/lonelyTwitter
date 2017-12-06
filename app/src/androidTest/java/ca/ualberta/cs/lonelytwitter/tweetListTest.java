b404b0abff740025de0d6324d6f7ecea5d3511a3
gsgould <gsgould@ualberta.ca>
2017-12-06 01:33:47 -0700

    Lab Exercise (JUnit and TDD)

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/tweetListTest.java
@@ -0,0 +1,39 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class tweetListTest extends ActivityInstrumentationTestCase2{
+    public tweetListTest(){
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("adding tweet ");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
+
+    public void testDelete(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test2");
+        tweets.add(tweet);
+        tweets.delete(tweet);
+        assertFalse(tweets.hasTweet(tweet));
+    }
+
+    public void testHasTweet(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test1");
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweeet(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test3");
+        list.add(tweet);
+        Tweet returnedTweet = list.getTweet(0);
+        assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+    }
+}
